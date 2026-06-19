9dda475bbcdc07a57cd0058348844cbf92031594
MembaMcetywa <iamemba10@gmail.com>
2026-06-19 10:21:33 +0200

    added a persistence layer using sqlite3

--- src/assets/assets.module.ts
+++ src/assets/assets.module.ts
@@ -4,9 +4,10 @@ import { ConfigModule } from '@nestjs/config';
 import { StorageModule } from '../storage/storage.module';
 import { AssetsController } from './assets.controller';
 import { AssetsService } from './assets.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [ConfigModule, StorageModule],
+  imports: [ConfigModule, StorageModule, DatabaseModule],
   controllers: [AssetsController],
   providers: [AssetsService],
   exports: [AssetsService],
