7f69d01fc188b2890e77971193bf47fd5440d959
MembaMcetywa <iamemba10@gmail.com>
2026-06-29 09:39:53 +0200

    add AlbumPresenter to AlbumsModule providers for enhanced data handling

--- src/albums/albums.module.ts
+++ src/albums/albums.module.ts
@@ -3,13 +3,14 @@ import { Module } from '@nestjs/common';
 import { LayoutModule } from '../layout/layout.module';
 import { AlbumsService } from './albums.service';
 import { AlbumsController } from './albums.controller';
+import { AlbumPresenter } from './album.presenter';
 import { RenderModule } from 'src/render/render.module';
 import { StorageModule } from '../storage/storage.module';
 import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   imports: [LayoutModule, RenderModule, DatabaseModule, StorageModule],
-  providers: [AlbumsService],
+  providers: [AlbumsService, AlbumPresenter],
   controllers: [AlbumsController],
   exports: [AlbumsService],
 })
