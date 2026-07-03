87f2d862a4f79e93502e605ffcfbfd9ff945c153
MembaMcetywa <iamemba10@gmail.com>
2026-07-03 15:58:34 +0200

    Add ImageNormalizerService to AssetsModule providers

--- src/assets/assets.module.ts
+++ src/assets/assets.module.ts
@@ -4,12 +4,13 @@ import { ConfigModule } from '@nestjs/config';
 import { StorageModule } from '../storage/storage.module';
 import { AssetsController } from './assets.controller';
 import { AssetsService } from './assets.service';
+import { ImageNormalizerService } from './image-normalizer.service';
 import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   imports: [ConfigModule, StorageModule, DatabaseModule],
   controllers: [AssetsController],
-  providers: [AssetsService],
+  providers: [AssetsService, ImageNormalizerService],
   exports: [AssetsService],
 })
 export class AssetsModule {}
